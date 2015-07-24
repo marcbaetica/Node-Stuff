c074d439a8b44fe9b3beae39fb4721c7fbd235f2
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-07-24 16:44:46 -0400

    added process.argv

--- Student Info GET App/app.js
+++ Student Info GET App/app.js
@@ -4,7 +4,26 @@ var profile = require("./profile.js"); //specifying .js is optional
 //change to see error
 //var username = "joshtimonen";
 
-profile.get("chalkers");
-profile.get("samarthpaboowal");
-profile.get("joshtimonen");
+//profile.get("chalkers");
+//profile.get("samarthpaboowal");
+//profile.get("joshtimonen");
 
+//putting users into an array
+/**
+var users = ['joshtimonen','chalkers'];
+users.forEach(function(username){ //with a callback function
+	profile.get(username);
+});
+//can also call directly users.forEach(profile.get);
+**/
+
+//there is a global object called process in node that allows us to access things like the current version of node and arguments passed in through the command line. insid this process there is an array called argv
+//console.log(process.argv); //call as node app.js joshtimonen chalkers
+
+
+var users = process.argv.slice(2); //we are interested starting with the second element of the array
+users.forEach(profile.get); //call as node app.js joshtimonen
+
+//TO IMPORVE:
+//add friendly messages when there is a parse error or connection error
+//make API call and create dropdown list to select which user
---
+++ ZipCode Weather Forecast/forecast.js
@@ -0,0 +1,3 @@
+//gets today's forecast based on the ZipCode
+//usage: node forecast.io 9020
+//to use: Forecast.io (has an API that can be used to get today's forecast)
