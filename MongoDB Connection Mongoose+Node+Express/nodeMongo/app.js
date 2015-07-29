75d218e5ed61e7b932ec9b6a114c6c6d8a40303f
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-07-28 22:23:12 -0400

    added express proj

--- MongoDB Connection Mongoose+Node+Express/nodeMongo/app.js
+++ MongoDB Connection Mongoose+Node+Express/nodeMongo/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'); //needed to access mongodb
 
 
 var routes = require('./routes/index');
--- MongoDB Connection Mongoose+Node+Express/nodeMongo/routes/index.js
+++ MongoDB Connection Mongoose+Node+Express/nodeMongo/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('ok');
+  console.log(req.query);
   //res.render('index', { title: 'Express' });
 });
 
