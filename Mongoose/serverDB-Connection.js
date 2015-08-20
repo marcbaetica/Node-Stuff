00627ac2fabe85c9b58f28394b1f0e159d575e4e
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-08-20 02:52:54 -0400

    fixed app settings

--- Mongoose/serverDB-Connection.js
+++ Mongoose/serverDB-Connection.js
@@ -1,27 +1,30 @@
-//dependencies and variables
+//dependencies and variable declaration
 var express = require('express');
-var port = process.env.PORT || 8000;
 var app = express();
-var destination;
-var route;
+var destiantion;
+
+//environments
+//setting env.port from in App Settings Table
+app.set('port', process.env.PORT || 8000);  //otherwise set as $ PORT=1234 node serverDB-connection.js
+app.set('dest', 'localhost');
+app.set('mainRoute', '/');
 
 //middleware
 app.use(function (req, res, next){
 	console.log('\nThe request method was: ' + req.method);
 	console.log('The requested URL was: http://' + req.hostname + '\n');
-	this.destination = req.hostname;
-	this.route = req.path;
-	console.log('Magic is still happening at http://' + req.hostname + ':' + port + req.path + ' !!\n');
+	console.log('Magic is still happening at http://' + app.get('dest') + ':' + app.get('port') + app.get('mainRoute') + ' !!\n');
 	next();
 });
 
 
 //routes
-app.get('/', function(req, res){
-	res.json({"name": "Marc"});
+app.get(app.get('mainRoute'), function(req, res){
+	res.json({name: "Marc"});
 });
 
 //binding app
-app.listen(port);
-console.log('Magic is happening at http://localhost' + /** destiantion +**/ ':' + port +/** route +**/ ' !!');
+app.listen(app.get('port'), function() {
+	console.log('Magic is happening at http://' + app.get('dest')  + ':' + app.get('port') + app.get('mainRoute') + ' !!');
+});
 
